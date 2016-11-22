22313b17d43f7b914bdca2dba573a91806f1c00b
一枚菜菜菜菜菜菜 <1127831462@qq.com>
2016-11-22 17:19:07 +0800

    修改

--- routes/user.js
+++ routes/user.js
@@ -2,22 +2,21 @@
  * Created by Administrator on 2016/11/7.
  */
 // var express = require('express');
-// var router = express.Router(); //调用express的路由文件
+// var router = express.Router();
 
-// //添加一个get方法
 // router.get('/', function(req, res){
 // 	res.send({
-// 		url: '/'
-// 	});
-// 	res.end();
-// });
-// //添加一个get方法
-// router.get('/index', function(req, res){
-// 	res.send({
-// 		url: '/index'
+// 		url: 'user/'
 // 	});
 // 	res.end();
 // });
 
-//指定默认模块
+// router.post('/userName', function(req, res){
+// 	res.send({
+// 		code: 0,
+// 		message: '保存成功'
+// 	});
+// 	res.end();
+// });
+
 // module.exports = router;
